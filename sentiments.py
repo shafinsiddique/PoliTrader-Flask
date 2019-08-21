9c9a16afa77e480d7210a4f3d0e2bc73dd842519
shafinsiddique <siddiqueshafin20@gmail.com>
2019-08-21 19:08:19 -0400

    - cleaned up code.

---
+++ sentiments.py
@@ -0,0 +1,19 @@
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+dataset = pd.read_csv("dataset.csv")
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(dataset['text'])
+model = pickle.load(open("classifier.sav","rb"))
+
+def get_sentiments(tweets):
+    df = pd.DataFrame(tweets, columns=['tweets'], index=None)
+    tweetsseries = df['tweets']
+
+    tweets_test = Tfidf_vect.transform(tweetsseries)
+
+    return model.predict(tweets_test)
+
+
+print(get_sentiments(["Hi! i am feeling great today!"]))
